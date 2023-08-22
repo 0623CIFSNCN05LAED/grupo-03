1e9f4c3fdb0de550b1ef31b6c77f0bbda5c6545e
fsica0308 <fsica0308@gmail.com>
2023-08-22 13:12:56 -0300

    Se agregaron archivos + comienzo del home

--- src/app.js
+++ src/app.js
@@ -8,8 +8,24 @@ app.use(express.static(path.join(__dirname, "../public")));
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
-  });
+});
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "views/home.html"));
-  });
+});
+
+app.get("/login", function (req, res) {
+  res.sendFile(path.join(__dirname, "views/login.html"));
+});
+
+app.get("/register", function (req, res) {
+  res.sendFile(path.join(__dirname, "views/register.html"));
+});
+
+app.get("/productCart", function (req, res) {
+  res.sendFile(path.join(__dirname, "views/productCart.html"));
+});
+
+app.get("/productDetail", function (req, res) {
+  res.sendFile(path.join(__dirname, "views/productDetail.html"));
+});
