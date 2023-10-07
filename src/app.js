685f30d4c0f5738b0db13b2902ccc330a162c4dc
antopagano <anto152@hotmail.es>
2023-10-07 11:00:20 -0300

    requiero en app el method-override

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
 const path = require("path");
 const express = require("express");
-const app = express();
-const mainRouter = require("./routes/main-router");
+const methodOverride = require ("method-override");
+
+
 const viewUtils = require("./utils/view-utils");
 
-app.use(express.static(path.join(__dirname, "../public")));
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../public"))); //// Necesario para los archivos estáticos en el folder /public
+app.use(methodOverride("_method")); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
+
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+
+const mainRouter = require("./routes/main-router"); // Rutas main
+app.use("/", mainRouter);
+
 
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
-
-app.set("view engine", "ejs");
-app.set("views", "./src/views");
-
-
-app.use("/", mainRouter);
