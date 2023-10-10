aa49a11df1a82c677cd4c496c581ceea2ccf4691
fsica0308 <fsica0308@gmail.com>
2023-10-09 23:26:19 -0300

    arreglos, create funcionando

--- src/routes/products.js
+++ src/routes/products.js
@@ -19,9 +19,6 @@ const productsController = require("../controllers/products-controller");
 // Recibir todos los productos
 router.get("/", productsController.home);
 
-// Detalle de un producto
-router.get("/:id/", productsController.detail);
-
 // Crear un producto
 router.get("/create/", productsController.create);
 router.post("/", upload.array("images"), productsController.store);
@@ -32,6 +29,9 @@ router.put("/:id", productsController.update);
 
 // Eliminar un producto
 router.delete("/:id", productsController.destroy);
+
+// Detalle de un producto
+router.get("/:id/", productsController.detail);
 
 
 module.exports = router;
