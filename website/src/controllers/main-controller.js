4cbc470503afc82a71450183e3cedfa50cd4629d
antopagano <anto152@hotmail.es>
2023-11-22 22:17:41 -0300

    website y dashboard creados

---
+++ website/src/controllers/main-controller.js
@@ -0,0 +1,75 @@
+const productServices = require("../services/product-services");
+const userServices = require("../services/user-services");
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  home: (req, res) => {
+    const featuredProducts = productServices.getFeaturedProducts();
+
+    res.render("home", {
+      featuredProducts,
+    });
+  },
+  showLogin: (req, res) => {
+    const errors = req.session.errors;
+    const oldData = req.session.oldData;
+    req.session.errors = null;
+    req.session.oldData = null;
+    res.render("login", {
+      errors: errors ? errors : null,
+      oldData: oldData ? oldData : null,
+    });
+  },
+  login: (req, res) => {
+    const data = req.body;
+    console.log(data);
+    const email = req.body.email;
+    const contrasena = req.body.contrasena;
+    const dataUser = userServices.validateUserLogin(email, contrasena);
+    if (dataUser !== null) {
+      req.session.userData = dataUser;
+      console.log('data session');
+    }
+    res.redirect("/");
+  },
+  showRegister: (req, res) => {
+    const errors = req.session.errors;
+    const oldData = req.session.oldData;
+    req.session.errors = null;
+    req.session.oldData = null;
+    res.render("register", {
+      errors: errors ? errors : null,
+      oldData: oldData ? oldData : null,
+    });
+  },
+  register: (req, res) => {
+    const data = req.body;
+    console.log(data);
+    req.session.userData = data;
+
+    /*const imagen = req.file;
+    const imagen_file_name = "/images/users/";
+    const imagenURL = imagen_file_name.concat(imagen.filename);
+    console.log(imagenURL);*/
+
+    const user = {
+      nombre: req.body.nombre,
+      apellido: req.body.apellido,
+      usuario: req.body.usuario,
+      email: req.body.email,
+      contraseña: req.body.contraseña,
+      images: req.file ? imagenURL : "/images/users/default-image.jpg",
+    };
+    userServices.registerUser(user);
+
+    res.redirect("/login");
+  },
+  productCart: (req, res) => {
+    res.render("productCart");
+  },
+  search: (req, res) => {
+    const keywords = req.query.keywords;
+    const foundProducts = productServices.searchProducts(keywords);
+    res.render("results", { foundProducts });
+  },
+};
---
+++ website/src/controllers/products-controller.js
@@ -0,0 +1,69 @@
+const productServices = require("../services/product-services");
+
+module.exports = {
+// Root - Show all products
+    home: (req, res) => { 
+        const products = productServices.getAllProducts();
+        res.render("products", { products });
+    },
+    detail: (req, res) => {
+        const id = req.params.id;
+        const product = productServices.getFormattedProduct(id);
+        res.render("productDetail", { product });
+    },
+    create: (req, res) => {
+        res.render("productCreate");
+    },
+    store: (req, res) => {
+        const ArrayReqFiles = req.files;
+        const ArrayImagenes = [];
+        const img_filename = "/images/products/";
+        for (let i = 0; i < ArrayReqFiles.length; i++) {
+            const url_filename = img_filename.concat((Math.floor(Math.random() * 1000)) + "-").concat(ArrayReqFiles[i].filename)
+            ArrayImagenes.push(url_filename);
+        }
+        console.log(ArrayImagenes);
+        const product = {
+            brand: req.body.brand,
+            name: req.body.name,
+            description: req.body.description,
+            detail: req.body.detail,
+            aditional: req.body.aditional,
+            images: req.files ? ArrayImagenes : "/images/products/default-image.jpg",
+            category: req.body.category,
+            price: Number(req.body.price),
+            discount: Number(req.body.discount),
+            rating: Number(req.body.rating),
+            colors: req.body.colors,
+            capacity: req.body.capacity,
+            os: req.body.os,
+            screen: req.body.screen,
+            camera: req.body.camera
+        };
+        productServices.createProduct(product);
+        res.redirect("/products");
+    },
+    edit: (req, res) => {
+        const id = req.params.id;
+        const product = productServices.getProduct(id);
+        res.render("productEdit", { product });
+    },
+    update: (req, res) => {
+        const product = req.body;
+        const id = req.params.id;
+        const image = req.file
+            ? req.file.filename
+            : productServices.getProduct(id).images;
+        productServices.updateProduct(id, product);
+        res.redirect("/products");
+    },
+    delete: (req, res) => {
+        res.render("productDelete");
+    },
+    destroy: (req, res) => {
+        const id = req.params.id;
+        console.log(`deleting product id: ${id}`);
+        productServices.deleteProduct(id);
+        res.redirect("/products");
+    },
+};
