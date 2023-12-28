36f77185a7e9fece0dbcd5ced1ba808aa90a10e1
LuiosD <luisdartayet@gmail.com>
2023-12-28 20:07:10 -0300

    fix: img register validation

--- src/validations/register.js
+++ src/validations/register.js
@@ -70,8 +70,9 @@ module.exports = [
   body("profile_picture")
     .custom((value, { req }) => {
       const file = req.file;
-      if (file == undefined) {
+      if (file === undefined) {
         throw new Error("Debe subir una imágen");
       }
+      return true;  
     }),
 ];
