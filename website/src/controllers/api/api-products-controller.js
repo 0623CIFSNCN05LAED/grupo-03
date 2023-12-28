7a025b3228e6c09820b1b985e8cf6c57f291609a
antopagano <anto152@hotmail.es>
2023-12-28 11:46:04 -0300

    api-product completa

--- website/src/controllers/api/api-products-controller.js
+++ website/src/controllers/api/api-products-controller.js
@@ -5,54 +5,34 @@ module.exports = {
     home: async (req, res) => {
 
         try {
-            const pageSize = 10;
-            const page = req.query.page || 1;
             const totalProductsCount = await productServices.getCountTotalProducts();
-            const offset = (page - 1) * pageSize;
             const totalProducts = await productServices.getAllProducts();
-            const allProducts = await productServices.getProductsLimit(
-                offset,
-                pageSize
-            );
-            const Brands = await productServices.getBrands();
-
-            // const totalPages = Math.ceil(totalProductsCount / pageSize);
-
-            function countProductsByBrand(allProducts) {
-                const prodsByBrand = {};
-                allProducts.forEach((product) => {
-                    const brand = product.brand_id;
-                    if (!prodsByBrand[brand]) {
-                        prodsByBrand[brand] = 1;
-                    } else {
-                        prodsByBrand[brand] += 1;
-                    }
-                });
-                return prodsByBrand;
-            }
-
-            const productsToApi = allProducts.map(
-                ({ id_product, name, description, brand }) => ({
-                    id_product,
-                    name,
-                    description,
-                    brands: [{ brand: brand }],
-                    detail: req.headers.host + "/api/products/" + id_product,
-                })
-            );
-
 
             let respuesta = {
                 meta: {
                     status: 200,
                     count: totalProductsCount,
-                    countByBrand: Brands,
-                    url: req.headers.host + req.originalUrl,
+                    countByBrand : {
+                        Samsung : totalProducts.filter((product) => product.brand.brand === "Samsung").length,
+                        Apple : totalProducts.filter((product) => product.brand.brand === "Apple").length,
+                        Motorola : totalProducts.filter((product) => product.brand.brand === "Motorola").length,
+                        Oppo : totalProducts.filter((product) => product.brand.brand === "Oppo").length,
+                        Huawei : totalProducts.filter((product) => product.brand.brand === "Huawei").length,
+                        Xiaomi : totalProducts.filter((product) => product.brand.brand === "Xiaomi").length,
+                        Google : totalProducts.filter((product) => product.brand.brand === "Google").length,
+
+                    }
                 },
-                products: productsToApi,
+                products: totalProducts.map(product =>({
+                    id : product.id_product,
+                    name : product.name,
+                    description: product.description,
+                    brand: product.brand,
+                    detail : `http://localhost:3030/api/products/${product.id_product}`
+                }))
             };
 
-            res.json(respuesta);
+            return res.json(respuesta);
         }
         catch (error) {
             res.status(500).json({ error: "Error al obtener los productos" });
@@ -64,46 +44,14 @@ module.exports = {
         const response = {
             meta: {
                 status: 200,
-                total: products.length,
-                url: "/api/products/:id"
+                // total: products.length,
+                url: `http://localhost:3030/api/products/${req.params.id}`
             },
-            data: {
-                id_product: product.id_product,
-                name: product.name,
-                description: product.description,
-                featured_desc: product.featured_desc,
-                featured: product.featured,
-                price: product.price,
-                priceWithDiscount: product.priceWithDiscount,
-                discount: product.discount,
-                rating: product.rating,
-                os: product.os,
-                screen: product.screen,
-                camera: product.camera,
-                id_brand: product.id_brand,
-                brand: product.brand,
-                image: product.image.map((image) => {
-                    return {
-                        id_image: image.id_image,
-                        url_image: "localhost:3030" + image.url_image,
-                    };
-                }),
-                colors: product.colors.map((color) => {
-                    return {
-                        id_color: color.id_color,
-                        color: color.color,
-                    };
-                }),
-                capacities: product.capacities.map((capacity) => {
-                    return {
-                        id_capacity: capacity.id_capacity,
-                        capacity: capacity.capacity,
-                    };
-                }),
-            }
+            data: product
         }
         res.json(response)
-    }
+    },
+    
 
 };
 
