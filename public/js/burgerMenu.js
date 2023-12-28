43856790baa760617fbd65a1c9416acf9dc5e5eb
Franco Sica <93335697+fsica0308@users.noreply.github.com>
2023-12-28 17:25:13 -0300

    Delete public directory

--- public/js/burgerMenu.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', () => {
-
-    const burgerMenu = document.querySelector('.fa-bars');
-    const menu = document.getElementById('menu');
-
-    burgerMenu.addEventListener('click', () => {
-        menu.classList.toggle('mostrarMenu');
-    })
-});    
